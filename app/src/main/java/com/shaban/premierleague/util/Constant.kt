c194157a5147c5c58d3302840a5fe24792ebdb3c
Mohamed Shaban <ms2951@fayoum.edu.eg>
2023-08-13 00:15:16 +0300

    add Constant in utils package that contains file name and columns numbers of csv file

---
+++ app/src/main/java/com/shaban/premierleague/util/Constant.kt
@@ -0,0 +1,28 @@
+package com.shaban.premierleague.util
+
+object Constant {
+    const val FILE_NAME = "premier_league_18_19.csv"
+
+    object ColumnIndex {
+        const val MATCH_DATE = 1
+        const val HOME_TEAM = 2
+        const val AWAY_TEAM = 3
+        const val FULL_TIME_HOME_GOALS = 4
+        const val FULL_TIME_AWAY_GOALS = 5
+        const val HALF_TIME_HOME_GOALS = 7
+        const val HALF_TIME_AWAY_GOALS = 8
+        const val REFEREE = 10
+        const val HOME_SHOTS = 11
+        const val AWAY_SHOTS = 12
+        const val HOME_SHOTS_ON_TARGET = 13
+        const val AWAY_SHOTS_ON_TARGET = 14
+        const val HOME_FOULS = 15
+        const val AWAY_FOULS = 16
+        const val HOME_CORNERS = 17
+        const val AWAY_CORNERS = 18
+        const val HOME_YELLOW_CARDS = 19
+        const val AWAY_YELLOW_CARDS = 20
+        const val HOME_RED_CARDS = 21
+        const val AWAY_RED_CARDS = 22
+    }
+}
