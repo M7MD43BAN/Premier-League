ba6d25d2eb4ce639a040000eb41a2263f28265cf
Mohamed Shaban <ms2951@fayoum.edu.eg>
2023-08-13 02:54:19 +0300

    enhance BaseActivity by call onDestroy to avoid memory leak and add getter to binding

--- app/src/main/java/com/shaban/premierleague/ui/BaseActivity.kt
+++ app/src/main/java/com/shaban/premierleague/ui/BaseActivity.kt
@@ -10,13 +10,14 @@ abstract class BaseActivity<VB : ViewBinding> : AppCompatActivity() {
     abstract val LOG_TAG: String
     abstract val bindingInflater: (LayoutInflater) -> VB
 
-    private var _binding: ViewBinding? = null
-    protected val binding = _binding as VB?
+    private var _binding: VB? = null
+    protected val binding: VB
+        get() = requireNotNull(_binding)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = bindingInflater(layoutInflater)
-        setContentView(requireNotNull(_binding).root)
+        setContentView(binding.root)
 
         setup()
         addCallBacks()
@@ -27,4 +28,9 @@ abstract class BaseActivity<VB : ViewBinding> : AppCompatActivity() {
     abstract fun addCallBacks()
 
     protected fun log(value: Any) = Log.i(LOG_TAG, value.toString())
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
